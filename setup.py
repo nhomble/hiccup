280d9f798c16096c323afa8781eaf925e5625c12
nicolas <nhomble@terpmail.umd.edu>
2017-11-05 18:59:29 -0500

    first stab at the setup.py file.. will update more later

---
+++ setup.py
@@ -0,0 +1,28 @@
+from distutils.core import setup
+
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+
+setup(
+    name='hiccup',
+    version='0.0.1',
+    packages=['hiccup'],
+    url='https://github.com/nhomble/hiccup',
+    license='MIT',
+    author='nicolas',
+    author_email='nhomble@terpmail.umd.edu',
+    description='my own img compression format for educational purposes',
+    long_description=readme(),
+    classifiers=[
+        'image compression :: python3'
+    ],
+    install_requires=[
+        "scipy",
+        "opencv-python",
+        "numpy",
+        "rawpy"
+    ],
+)
