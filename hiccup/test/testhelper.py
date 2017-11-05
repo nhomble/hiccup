59ae39a3305f5a88a56464ae6c6698ce5010f88c
nicolas <nhomble@terpmail.umd.edu>
2017-11-05 13:40:01 -0500

    test helpers

---
+++ hiccup/test/testhelper.py
@@ -0,0 +1,11 @@
+import numpy as np
+import cv2
+
+
+def dumb_rgb(N: int):
+    parts = [
+        np.full((N, N), 1, dtype=np.uint8),
+        np.full((N, N), 2, dtype=np.uint8),
+        np.full((N, N), 3, dtype=np.uint8)
+    ]
+    return cv2.merge(parts)
