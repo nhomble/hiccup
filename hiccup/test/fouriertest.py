c23e5559139b21018f201b82cc1f2a6a880fbaa3
nicolas <nhomble@terpmail.umd.edu>
2017-11-05 19:06:11 -0500

    null test

--- hiccup/test/fouriertest.py
+++ hiccup/test/fouriertest.py
@@ -20,3 +20,10 @@ class FourierTransformTest(unittest.TestCase):
         splits = cv2.split(out)
         lumin = splits[0]
         self.assertEqual(lumin[0][0], np.max(lumin))
+
+    def test_null(self):
+        img = np.zeros((120, 80, 3)).astype(np.uint8)
+        transform = FourierTransform()
+        out = transform.compress(img)
+        lumin = cv2.split(out)[0]
+        self.assertEqual(0, np.sum(lumin))
