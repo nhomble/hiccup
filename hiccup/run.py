739277a2e1284fe54b806017aa6300187fe13bf9
nicolas <nhomble@terpmail.umd.edu>
2017-11-27 02:42:03 -0500

    no need for objects

--- hiccup/run.py
+++ hiccup/run.py
@@ -7,36 +7,8 @@ def compress(path, output, style):
     if style is None:
         print("Style cannot be none")
         return
-    c = Compressor.load(path)
-    c.shrink(style, output)
+    img = iohelper.open_raw_img(path)
 
 
 def decompress(path, output):
-    d = Decompressor.load(path)
-    d.explode(output)
-
-
-class Compressor:
-    @staticmethod
-    def load(img_path: str):
-        img = iohelper.open_raw_img(img_path)
-        return Compressor(img)
-
-    def __init__(self, img: np.ndarray):
-        self._img = img
-
-    def shrink(self, style: str, output_path: str):
-        pass
-
-
-class Decompressor:
-    @staticmethod
-    def load(img_path: str):
-        # read binary
-        return Decompressor(None)
-
-    def __init__(self, hic: hic.HicImage):
-        self._hic = hic
-
-    def explode(self, output_path):
-        pass
+    pass
