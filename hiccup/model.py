1bc2d5f64f28ee98a227a808f469981711f92aea
nicolas <nhomble@terpmail.umd.edu>
2017-12-01 00:55:29 -0500

    put some basic models for type safety

---
+++ hiccup/model.py
@@ -0,0 +1,22 @@
+import enum
+
+"""
+Basically house the enumerations here and other broad modelling components
+"""
+
+
+class Compression(enum.Enum):
+    """
+    The broad image compression schemes implements in hiccup
+    """
+    JPEG = "JPEG"
+    HIC = "HIC"  # basically wavelet
+
+
+class Coefficient(enum.Enum):
+    """
+    Applicable in JPEG compression with the Discrete Fourier transform. The DC component represents the coefficient of
+    the sinusoid with no frequency. The AC components refer to coefficients of sinusoids with different frequency.
+    """
+    DC = "DC"
+    AC = "AC"
