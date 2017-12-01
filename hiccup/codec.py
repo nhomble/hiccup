164f195cb6d2c12fc2c0795e5533076b84e6231a
nicolas <nhomble@terpmail.umd.edu>
2017-12-01 00:19:33 -0500

    run length encoding

--- hiccup/codec.py
+++ hiccup/codec.py
@@ -1,5 +1,7 @@
+import functools
 import numpy as np
 
+import hiccup.utils as utils
 import hiccup.transform as transform
 
 """
@@ -7,10 +9,6 @@ Encoding/Decoding functionality aka
     Run Length encoding
     Huffman Encoding
 """
-
-table = {
-
-}
 
 
 def differential_coding(blocks: np.ndarray):
@@ -27,6 +25,32 @@ def differential_coding(blocks: np.ndarray):
             ret.append(dc - dc_comps[i])
             i += 1
     return ret
+
+
+def run_length_coding(matrix: np.ndarray):
+    """
+    Come up with the run length encoding for a matrix
+    """
+    zigzag = transform.zigzag(matrix)
+
+    def reduction(agg, next):
+        if next == 0:
+            agg[-1]["zeros"] += 1
+            return agg
+
+        if "value" not in agg[-1]:
+            agg[-1]["value"] = next
+
+        agg.append({"zeros": 0})
+        return agg
+
+    rl = functools.reduce(reduction, zigzag, [{"zeros": 0}])
+
+    # If the last element has value then it was 0! That is a special tuple, (0,0)
+    if "value" not in rl[-1]:
+        rl[-1] = {"zeros": 0, "value": 0}
+
+    return [dict(d, bits=utils.num_bits_for_int(d["value"])) for d in rl]
 
 
 def encode(array: np.ndarray):
--- hiccup/test/codectest.py
+++ hiccup/test/codectest.py
@@ -11,3 +11,20 @@ class CodecTest(unittest.TestCase):
         blocks = transform.split_matrix(matrix, 4)
         dc = codec.differential_coding(blocks)
         self.assertEqual([0, 4, 0, 4], dc)
+
+    def test_run_length(self):
+        matrix = np.array([
+            [99, -59, 0, 7, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [12, -2, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0]
+        ])
+        rl = codec.run_length_coding(matrix)
+        expected = [{'zeros': 0, 'value': 99, 'bits': 7}, {'zeros': 1, 'value': -59, 'bits': 6},
+                    {'zeros': 6, 'value': 7, 'bits': 3}, {'zeros': 4, 'value': 12, 'bits': 4},
+                    {'zeros': 1, 'value': -2, 'bits': 2}, {'zeros': 0, 'value': 0, 'bits': 0}]
+        self.assertEqual(expected, rl)
