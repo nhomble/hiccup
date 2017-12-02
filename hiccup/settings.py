872c8919d8bc3a52872b62cd1ac7bb84bd3b9100
nicolas <nhomble@terpmail.umd.edu>
2017-12-02 16:09:46 -0500

    jpeg block size (always 8)

--- hiccup/settings.py
+++ hiccup/settings.py
@@ -8,9 +8,12 @@ I suppose once I expose all of the settings it will be easier to organize.
 """
 
 DEBUG = True
+
 WAVELET = model.Wavelet.HAAR
 WAVELET_QUALITY_FACTOR = 1
 WAVELET_SUBBAND_QUANTIZATION_MULTIPLIER = 0
 WAVELET_THRESHOLD = 8
 WAVELET_NUM_LEVELS = 3
 WAVELET_TILES = 8
+
+JPEG_BLOCK_SIZE = 8 # never going to change this since this would require an update to our qnt tables
