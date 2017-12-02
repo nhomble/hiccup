df08a5e6628bcb72313f35d0e40611e8ce7e810a
nicolas <nhomble@terpmail.umd.edu>
2017-12-02 02:33:17 -0500

    qfactor is a setting

--- hiccup/compression.py
+++ hiccup/compression.py
@@ -7,6 +7,7 @@ import hiccup.hicimage as hic
 import hiccup.quantization as qnt
 import hiccup.model as model
 import hiccup.settings as settings
+import hiccup.utils as utils
 
 """
 Houses the entry functions to either compression algorithm
@@ -46,8 +47,16 @@ def wavelet_compression(rgb_image: np.ndarray) -> hic.HicImage:
     [t_gray, t_color_1, t_color_2] = [transform.linearize_subband(qnt.subband_quantize(transform.subband_view(b))) for b
                                       in [t_gray, t_color_1, t_color_2]]
 
-    [t_gray, t_color_1, t_color_2] = [transform.threshold_channel_by_quality(b) for b in
-                                      [t_gray, t_color_1, t_color_2]]
+    [t_gray, t_color_1, t_color_2] = [transform.threshold_channel_by_quality(
+        b, q_factor=settings.WAVELET_QUALITY_FACTOR) for b in [t_gray, t_color_1, t_color_2]]
+
+    utils.debug_img(cv2.cvtColor(
+        cv2.merge([
+            transform.wavelet_merge_resolutions(t_gray, model.Wavelet.DAUBECHIE),
+            transform.wavelet_merge_resolutions(t_color_1, model.Wavelet.DAUBECHIE),
+            transform.wavelet_merge_resolutions(t_color_2, model.Wavelet.DAUBECHIE)
+        ]).astype(np.uint8), cv2.COLOR_YCrCb2RGB
+    ))
     t_gray = [qnt.round_quantize(b) for b in t_gray]
     t_color_1 = [qnt.round_quantize(b) for b in t_color_1]
     t_color_2 = [qnt.round_quantize(b) for b in t_color_2]
--- hiccup/settings.py
+++ hiccup/settings.py
@@ -3,7 +3,10 @@ import hiccup.model as model
 """
 Ok.. it wasn't meant to be like this, but I don't have the time or inclination to properly encapsulate these things. So
 here is a global config file that can get modified at runtime when the user is belching.
+
+I suppose once I expose all of the settings it will be easier to organize.
 """
 
 DEBUG = True
 WAVELET = model.Wavelet.DAUBECHIE
+WAVELET_QUALITY_FACTOR = 1
