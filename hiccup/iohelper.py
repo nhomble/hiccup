b76ad824be43fb9f008b9019606650c45eb1bf74
nicolas <nhomble@terpmail.umd.edu>
2017-12-03 20:14:24 -0500

    more bit stuff

--- hiccup/iohelper.py
+++ hiccup/iohelper.py
@@ -16,18 +16,44 @@ def open_raw_img(path):
     return rgb
 
 
-def bin_string(i: int):
+def bin_string(i: int) -> str:
     """
     Binary string
     """
     return bin(i)[2:]
 
 
-def bin_string_as_bytes(s):
+def bin_string_as_bytes(s: str) -> bytearray:
     """
     Need to represent each bit as an actual bit instead of wasting a byte per char. Also I have to respect the zeros
     """
     assert len(s) > 0 and len(s) % 8 == 0  # if not, I probably forgot to pad upstream in HIC
     i = int(s, 2)
-    out = i.to_bytes(len(s) // 8, byteorder='big')
+    out = bytearray(i.to_bytes(len(s) // 8, byteorder='big'))
     return out
+
+
+def padded_bs_2_bytes(s: str) -> bytearray:
+    """
+    I NEED TO BE BYTE ALIGNED, otherwise how do I know what's going on
+    """
+    padding = 8 - (len(s) % 8)
+    s += ("0" * padding)
+    binary = bin_string_as_bytes(s)
+    binary.extend(chr(padding).encode('ascii'))
+    # so first byte tells me the padding
+    return binary
+
+
+def padded_bytes_2_bs(bites: bytearray) -> str:
+    padding = bites[-1]
+    b = bites[:-1]
+    i = int.from_bytes(b, byteorder='big')
+    s = bin_string(i)
+    # any diff should be leading
+    res = s[:-padding]
+    if len(res) + padding % 8 != 0:
+        # forward padd
+        diff = 8 - (len(res) + padding % 8)
+        res = ("0" * diff) + res
+    return res
--- hiccup/test/iohelpertest.py
+++ hiccup/test/iohelpertest.py
@@ -14,3 +14,19 @@ class IOHelperTest(unittest.TestCase):
     def test_s2b_assertion(self):
         self.assertRaises(AssertionError, io.bin_string_as_bytes, '0')
         self.assertRaises(AssertionError, io.bin_string_as_bytes, '')
+
+    def _check_pb(self, s):
+        out = io.padded_bs_2_bytes(s)
+        invert = io.padded_bytes_2_bs(out)
+        self.assertEqual(s, invert, msg=s)
+
+    def test_padding_and_back(self):
+        cases = [
+            "01",
+            "0000",
+            "1010000",
+            "00000",
+            "000111"
+        ]
+        for case in cases:
+            self._check_pb(case)
